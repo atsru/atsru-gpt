4c128ec76b0b642bfac74aa5ee59f863ab179faa
andrei <ua.andrei@gmail.com>
2023-05-11 17:55:53 +0300

    added css reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
+import "sanitize.css";
+import "normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <Provider store={store}>
